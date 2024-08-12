f1b52742626aa3aebfff3fb6dc6ce2da53ef96f3
vikas9872 <shanabhogvikas648.com>
2024-08-12 20:23:06 +0530

    New

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,32 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import './Navbar.css'
 const Navbar = () => {
-    const [menu,setMenu]=useState('home')
+    const [disp, setDisp] = useState(false);
+    const getMenuStyle = (disp) => {
+        if (document.documentElement.clientWidth <= 426) {
+            return { right: !disp && "-100%" }
+        }
+    }
     return (
-        <div className='navbar'>
-            <div className="logo"><h1>FoooodCourttttt...</h1></div>
-            <ul className='navbarmenu'>
-                <li onClick={()=>setMenu("home")} className={menu === "home" ? "active" : ""}>Home</li>
-                <li onClick={()=>setMenu("menu")} className={menu === "menu" ? "active" : ""}>Menu</li>
-                <li onClick={()=>setMenu("tspecials")} className={menu === "tspecials" ? "active" : ""}>Today's Specials</li>
-                <li onClick={()=>setMenu("contact")} className={menu === "contact" ? "active" : ""}>Contact Us</li>
-            </ul>
-            <div className="navbarsignin">
-                <button className='navbarsigninbtn'>Sign In</button>
+        <>
+            <div className="navbar">
+                <div className="navbarLeft">
+                    <img src="./Images/logo.svg" alt="Logo" />
+                </div>
+                <div className="navbarCenter" style={getMenuStyle(disp)}>
+                    <ul>
+                        <li>Home</li>
+                        <li>Menu</li>
+                        <li>Contact Us</li>
+                        <button className='signInbth'>Sign In</button>
+
+                    </ul>
+                </div>
+                <div className="navbarRight">
+                    <img src="./Images/hamburgericon.png" className='hicon' alt="cannot be displayed" onClick={() => setDisp((prev) => !prev)} />
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
